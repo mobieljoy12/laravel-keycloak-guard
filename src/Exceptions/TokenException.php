f17cefdf1f69c942db32199a9dbc4fbb2aa1f536
jaspervdkooi <jaspervdkooi@promocat.nl>
2024-07-19 09:55:51 +0200

    Adding render function to TokenException in order to return an Unauthorized response

--- src/Exceptions/TokenException.php
+++ src/Exceptions/TokenException.php
@@ -2,6 +2,14 @@
 
 namespace KeycloakGuard\Exceptions;
 
+use Illuminate\Http\JsonResponse;
+
 class TokenException extends KeycloakGuardException
 {
+    public function render($request): JsonResponse
+    {
+        return response()->json([
+            'error' => $this->message,
+        ], 401);
+    }
 }
